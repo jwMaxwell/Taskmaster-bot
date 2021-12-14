08b35ace17d7a4da9976fdf54412c6d911586e57
Brandon Ingli <brandon@brandoningli.com>
2021-12-13 20:33:35 -0600

    Remove Spike reference

--- src/CustomDiscordWebhookTransport.js
+++ src/CustomDiscordWebhookTransport.js
@@ -1,7 +1,7 @@
 const DiscordWebhookTransport = require("@typicalninja21/discord-winston");
 
 // prettier-ignore
-module.exports = class SpikeDiscordWebhookTransport extends DiscordWebhookTransport {
+module.exports = class CustomDiscordWebhookTransport extends DiscordWebhookTransport {
   log(info, callback) {
     if (info.postToDiscord == false) return callback();
     this.postToWebhook(info);
